fa1851afb147c649f96d09e9d26ee01e2926f155
Fernando Hissataka <fhissataka@gmail.com>
2022-04-19 11:56:34 -0300

    Obtendo um post por id

---
+++ src/main/java/com/fhissa/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.fhissa.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fhissa.workshopmongo.domain.Post;
+import com.fhissa.workshopmongo.repository.PostRepository;
+import com.fhissa.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
+
